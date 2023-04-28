db9562670ad7e73e6cceede70302490dbfeb0587
maggomor <maggomorizon@googlemail.com>
2023-04-28 07:08:46 -0400

    testcommit

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_accounts():
     """
@@ -78,6 +79,7 @@ def list_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def get_accounts(account_id):
     """
@@ -94,6 +96,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
@@ -114,6 +117,7 @@ def update_accounts(account_id):
 # DELETE AN ACCOUNT
 ######################################################################
 
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
@@ -123,7 +127,7 @@ def delete_accounts(account_id):
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
     if account:
-            account.delete()
+        account.delete()
     return "", status.HTTP_204_NO_CONTENT
 
 
